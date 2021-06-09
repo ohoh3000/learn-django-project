43ef88d2e1efedc359ee8ed658ada90b7c149cef
iengu <ien-gu@qq.com>
2021-06-09 16:45:14 +0800

    first commit

---
+++ polls/models.py
@@ -0,0 +1,27 @@
+from django.db import models
+from django.utils import timezone
+import datetime
+
+
+
+# 创建数据模型.
+
+# 问题
+class Question(models.Model):
+    question_text = models.CharField(max_length=200)
+    pub_date = models.DateTimeField('date published')
+
+    def __str__(self):
+        return self.question_text
+    
+    def was_published_recently(self):
+        return self.pub_date >= timezone.now() - datetime.timedelta(days=1)
+
+# 选项
+class Choice(models.Model):
+    question = models.ForeignKey(Question, on_delete=models.CASCADE)
+    choice_text = models.CharField(max_length=200)
+    votes = models.IntegerField(default=0)
+
+    def __str__(self):
+        return self.choice_text
